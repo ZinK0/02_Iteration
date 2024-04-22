060dc27917901bdbad5e5f5289ab669541a976f1
Zin KO <mrzinko.it@gmail.com>
2024-04-23 02:23:43 +0630

    Finished Module 3:Iteration

---
+++ script.js
@@ -0,0 +1,8 @@
+// TODO: Write Your JavaScript Code Here
+let students = ["Mg Mg", "Aung Aung", "Hla Hla", "Mya Mya", "Aye Aye"];
+
+console.log(students.length);
+
+for (let i = 0; i < students.length; i++) {
+  console.log("Great to see you, ", students[i], "!");
+}
